598c09481d9640ba30bc90c354ec447fb7c13bde
Natiel Traore <you@example.com>
2022-11-20 00:45:04 +0200

    Worked on calculator and app files.

--- js/application.js
+++ js/application.js
@@ -21,21 +21,12 @@ document.getElementById('mode').addEventListener('click', () => {
 
 })
 
-
 const values = [];
-function pass_value(evt) {
-    values.push(evt);
-}
-
-const opers_btns =  document.getElementsByClassName('opers')
-const num_btns = document.getElementsByClassName('btn-num')
-
-for (let i = 0; i < opers_btns.length; i++) {
-    const oper_btn = opers_btns[i];
-    oper_btn.addEventListener('click', function(){pass_value(oper_btn.id)});
-}
-
-for (let i = 0; i < num_btns.length; i++) {
-    const num_btn = num_btns[i];
-    num_btn.addEventListener('click', function(){pass_value(num_btn.id)});
+const buttons = document.getElementsByClassName('btns');
+for (let i = 0; i < buttons.length; i++) {
+    const btn = buttons[i];
+    btn.addEventListener('click', () => {
+    values.push(btn.id);
+    console.log(values)
+})
 }
--- js/caculator.js
+++ js/caculator.js
@@ -1,27 +1,61 @@
 
 
-// BUTTONS INFO
-const buttons = document.getElementsByClassName('btns');
-function displayButtonInfo(val)  {
-        val.addEventListener('click', alert(val.id));       
+let currentValue = 0
+let firstOperand = ''
+let secondOperand = ''
+let currentOperator = ''
+
+const btns = document.getElementsByClassName('btns');
+
+for (let i = 0; i < btns.length; i++) {
+    const btn = btns[i];
+    btn.addEventListener('click', parseKey)
+}
+
+
+// CHECKS INPUT TYPE
+
+function type(input_key) {
+    let opers = '+-/x';
+    if (opers.indexOf(input_key) > -1) {
+        return 'operator';
+    } else if (input_key == '=') {
+        return 'eval';
+    } else {
+        return 'number';
     }
     
-for (let i = 0; i < buttons.length; i++) {
-    const btn = buttons[i];
-    btn.addEventListener('click', () => {
-        alert(btn.id);
-    })
 }
 
-
-// Checks if a given key is operator or number
-function is_oper(input_key) {
-    opers = ['=', '+', '-', '*', '/']
-    if (opers.indexOf(input_key) > -1) {
-        return true
+function parseKey(vals) {
+    let l = vals.length;
+    for (let i = 0; i < l; i++) {
+        if (type(vals[i]) == 'number') {
+            if (currentOperator == '') {
+                firstOperand += vals[i];
+            } else {
+                secondOperand += vals[i];
+            } 
+        } else {
+            if (firstOperand !== '' && secondOperand !== '') {
+                cal();
+                currentOperator = vals[i]
+            } else {
+                currentOperator = vals[i]
+            }
+        }
     }
-    return false
 }
+
+let a = document.querySelector(".eq")
+a.addEventListener('click', () => {
+    let curRes = values.join();
+    parseKey(values)
+    cal()
+    console.log(firstOperand)
+    console.log(secondOperand)
+    console.log(currentOperator)
+    alert(eval(firstOperand + currentOperator + secondOperand));
 
 
 function cal(o1, o2, a) {
@@ -43,38 +77,12 @@ function cal(o1, o2, a) {
     }    
 }
 
-const CalState = {
-    cur_val: 0,
-    first_op: '',
-    second_op: '',
-    action: '',
+// const CalState = {
+//     cur_val: 0,
+//     first_op: '',
+//     second_op: '',
+//     action: '',
     
-    parseKey: function(vals){
-        
-        let l = vals.length
-        for (let i = 0; i < l ; i++) {
-            
-            if (is_oper(vals[i] == false)) {
-                if (this.action == ''){
-                    this.first_op += vals[i]
-                } else {this.second_op += vals[i]}
-            }
-            else {
-                
-                if (this.first_op != '' && this.second_op != '') {
-                    if (this.action == '') {
-                        this.action = vals[i]
-                    }
-                    else {
-                        let cur_res = cal(this.first_op, this.second_op, this.action)
-                        this.action = vals[i]
-                    }
-                }
-            }
-        }  
-    }
-
-    
-}
+//     parseKey: 
 
 
